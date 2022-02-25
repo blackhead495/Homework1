625841d13127db0585c5767d43efd7f508abb051
Dzhalilov <df@dealmed.ru>
2022-02-25 17:26:40 +0300

    Add first text to project

--- WorkSolution1/Work1/Program.cs
+++ WorkSolution1/Work1/Program.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Какое из двух введеных чисел больше
 
-Console.WriteLine("Hello, World!");
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2) Console.WriteLine("max = " + num1);
+else            Console.WriteLine("max = " + num2);
